351f3f6dbe79e94e67de9e9f7fca047e74c07bad
Shail Mehta <shailmehta25@gmail.com>
2025-01-25 12:24:44 +0530

    Changed Inline Documentation Order (#68879)
    * Changed Inline Documentation Order
    
    * Changed Inline Documentation Order
    
    Co-authored-by: shail-mehta <shailu25@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>

--- packages/editor/src/store/selectors.js
+++ packages/editor/src/store/selectors.js
@@ -1234,10 +1234,10 @@ export const isEditorPanelOpened = createRegistrySelector(
 /**
  * Returns the current selection start.
  *
+ * @deprecated since Gutenberg 10.0.0.
+ *
  * @param {Object} state
  * @return {WPBlockSelection} The selection start.
- *
- * @deprecated since Gutenberg 10.0.0.
  */
 export function getEditorSelectionStart( state ) {
 	deprecated( "select('core/editor').getEditorSelectionStart", {
@@ -1250,10 +1250,10 @@ export function getEditorSelectionStart( state ) {
 /**
  * Returns the current selection end.
  *
+ * @deprecated since Gutenberg 10.0.0.
+ *
  * @param {Object} state
  * @return {WPBlockSelection} The selection end.
- *
- * @deprecated since Gutenberg 10.0.0.
  */
 export function getEditorSelectionEnd( state ) {
 	deprecated( "select('core/editor').getEditorSelectionStart", {
